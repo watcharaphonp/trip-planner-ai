1cdd3c3f5bc7ad7dcca2ac0610841592ebdfc936
watcharaphonp <watcharaphon@treg.co.th>
2024-03-21 12:07:32 +0700

    add make file and duckduckgo search tool

---
+++ makefile
@@ -0,0 +1,32 @@
+ENV_NAME = "venv"
+DEPENDENCIES_FILE_NAME = "requirements.txt"
+
+default: run
+
+build: 
+	poetry config warnings.export false
+	poetry export --without-hashes -f $(DEPENDENCIES_FILE_NAME) -o $(DEPENDENCIES_FILE_NAME)
+	python3 -m venv $(ENV_NAME)
+
+prepare: 
+	python3 -m pip install --user pipx
+	python3 -m pipx ensurepath
+	pipx install poetry
+	pipx inject poetry poetry-plugin
+	make build
+	make activate
+
+activate: 
+	. $(ENV_NAME)/bin/activate
+
+u.poetry: pipx upgrade poetry
+
+run: r
+r:
+	make activate
+	python3 main.py
+
+install: i
+i:
+	make activate
+	pip install --no-cache-dir -r $(DEPENDENCIES_FILE_NAME)
---
+++ tools/search_duckduckgo_tools.py
@@ -0,0 +1,33 @@
+import json
+import requests
+
+from langchain.tools import tool
+
+class SearchTools():
+    @tool("Search the internet")
+    def search_internet(query):
+        """Useful to search the internet
+        about a given topic and return relevant results"""
+        top_result_to_return = 4
+        url = "https://api.duckduckgo.com/"
+        params = {
+            "q": query,
+            "format": "json"
+        }
+        response = requests.get(url, params=params)
+        
+        if response.status_code != 200:
+            return "Sorry, there was an error with the search request."
+        
+        results = response.json()['RelatedTopics']
+        string = []
+        for result in results[:top_result_to_return]:
+            try:
+                string.append('\n'.join([
+                    f"Title: {result['Text']}", f"Link: {result['FirstURL']}",
+                    f"Snippet: {result['Result']}", "\n-----------------"
+                ]))
+            except KeyError:
+                continue
+
+        return '\n'.join(string)

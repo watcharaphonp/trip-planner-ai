52b5ccc30b9df1fcb23968d244a4c6d6805fe99e
watcharaphonp <watcharaphon@treg.co.th>
2024-03-21 14:01:43 +0700

    implement ducduckgo search

--- agents.py
+++ agents.py
@@ -2,7 +2,7 @@ from crewai import Agent
 from textwrap import dedent
 from langchain_openai import ChatOpenAI
 
-from tools.search_tools import SearchTools
+from tools.search_duckduckgo_tools import SearchTools
 from tools.calculator_tools import CalculatorTools
 
 """
--- makefile
+++ makefile
@@ -12,8 +12,7 @@ prepare:
 	python3 -m pip install --user pipx
 	python3 -m pipx ensurepath
 	pipx install poetry
-	pipx inject poetry poetry-plugin
-	make build
+	make buildmake 
 	make activate
 
 activate: 

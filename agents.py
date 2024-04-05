0b99412363339a840f7e96db11331e6b93f34c15
watcharaphonp <watcharaphon@treg.co.th>
2024-04-05 09:41:27 +0700

    update litellm config

--- agents.py
+++ agents.py
@@ -1,6 +1,6 @@
 from crewai import Agent
 from textwrap import dedent
-
+from langchain.agents import Tool
 from tools.search_tools import SearchTools
 from tools.search_duckduckgo_tools import DuckDuckGoSearchTools
 from tools.calculator_tools import CalculatorTools
@@ -16,56 +16,28 @@ from langchain_community.chat_models import ChatLiteLLM
 # Load environment variables from .env file
 load_dotenv()
 
-"""
-Creating Agents Cheat Sheet:
-- Think like a boss. Work backwards from the goal and think which employee 
-    you need to hire to get the job done.
-- Define the Captain of the crew who orient the other agents towards the goal. 
-- Define which experts the captain needs to communicate with and delegate tasks to.
-    Build a top down structure of the crew.
-
-Goal:
-- Create a 7-day travel itinerary with detailed per-day plans,
-    including budget, packing suggestions, and safety tips.
-
-Captain/Manager/Boss:
-- Expert Travel Agent
-
-Employees/Experts to hire:
-- City Selection Expert 
-- Local Tour Guide
-
-
-Notes:
-- Agents should be results driven and have a clear goal in mind
-- Role is their job title
-- Goals should actionable
-- Backstory should be their resume
-"""
-
 
 class TravelAgents:
 
     def __init__(self):
         # Set LLM
-        self.llm = ChatLiteLLM(model="claude-3-haiku-20240307", max_tokens=1000)
+        self.llm = ChatLiteLLM(
+            model="claude-3-haiku-20240307",
+            max_tokens=4096,
+        )
 
     def expert_travel_agent(self):
         return Agent(
             role="Expert Travel Agent",
             backstory=dedent(
-                f"""Expert in travel planning and logistics. 
-                I have decades of expereince making travel iteneraries."""
+                f"""Expert in travel planning and logistics. I have decades of expereince making travel iteneraries."""
             ),
-            goal=dedent(
-                f"""
-                        Create a travel itinerary with detailed per-day plans,
-                        include budget, packing suggestions, and safety tips.
-                        """
-            ),
+            goal=dedent(f"""Create a travel itinerary with detailed per-day plans"""),
             tools=[SearchTools.search_internet, CalculatorTools.calculate],
             verbose=True,
             llm=self.llm,
+            memory=True,
+            max_rpm=3,
         )
 
     def city_selection_expert(self):
@@ -82,14 +54,15 @@ class TravelAgents:
             ],
             verbose=True,
             llm=self.llm,
+            memory=True,
+            max_rpm=3,
         )
 
     def local_tour_guide(self):
         return Agent(
             role="Local Tour Guide Agent",
             backstory=dedent(
-                f"""Knowledgeable local guide with extensive information
-        about the city, it's attractions and customs"""
+                f"""Knowledgeable local guide with extensive information about the city, it's attractions and customs"""
             ),
             goal=dedent(f"""Provide the BEST insights about the selected city"""),
             tools=[
@@ -97,4 +70,6 @@ class TravelAgents:
             ],
             verbose=True,
             llm=self.llm,
+            memory=True,
+            max_rpm=3,
         )
--- tasks.py
+++ tasks.py
@@ -5,48 +5,6 @@ from utils.logging import logger
 import markdown
 import json
 
-"""
-Creating Tasks Cheat Sheet:
-- Begin with the end in mind. Identify the specific outcome your tasks are aiming to achieve.
-- Break down the outcome into actionable tasks, assigning each task to the appropriate agent.
-- Ensure tasks are descriptive, providing clear instructions and expected deliverables.
-
-Goal:
-- Develop a detailed itinerary, including city selection, attractions, and practical travel advice.
-
-Key Steps for Task Creation:
-1. Identify the Desired Outcome: Define what success looks like for your project.
-    - A detailed 7 day travel itenerary.
-
-2. Task Breakdown: Divide the goal into smaller, manageable tasks that agents can execute.
-    - Itenerary Planning: develop a detailed plan for each day of the trip.
-    - City Selection: Analayze and pick the best cities to visit.
-    - Local Tour Guide: Find a local expert to provide insights and recommendations.
-
-3. Assign Tasks to Agents: Match tasks with agents based on their roles and expertise.
-
-4. Task Description Template:
-  - Use this template as a guide to define each task in your CrewAI application. 
-  - This template helps ensure that each task is clearly defined, actionable, and aligned with the specific goals of your project.
-
-  Template:
-  ---------
-  def [task_name](self, agent, [parameters]):
-      return Task(description=dedent(f'''
-      **Task**: [Provide a concise name or summary of the task.]
-      **Description**: [Detailed description of what the agent is expected to do, including actionable steps and expected outcomes. This should be clear and direct, outlining the specific actions required to complete the task.]
-
-      **Parameters**: 
-      - [Parameter 1]: [Description]
-      - [Parameter 2]: [Description]
-      ... [Add more parameters as needed.]
-
-      **Note**: [Optional section for incentives or encouragement for high-quality work. This can include tips, additional context, or motivations to encourage agents to deliver their best work.]
-
-      '''), agent=agent)
-
-"""
-
 
 class TravelTasks:
     def __init__(self, job_id):
@@ -54,29 +12,18 @@ class TravelTasks:
 
     def append_event_callback(self, task_name, task_output):
         logger.info("Callback called: %s", task_output)
-        append_event(self.job_id, task_output.exported_output)
+        append_event(self.job_id, task_name + " has been finished")
         append_task_output(self.job_id, task_output.exported_output, task_name)
 
     def plan_itinerary(self, agent, city, travel_dates, interests):
         task_name = "plan_itinerary"
         return Task(
             description=dedent(
-                f"""
-            **Task**: Develop a Travel Itinerary
-            **Description**: Expand the city guide into a travel itinerary with detailed 
-                per-day plans, including weather forecasts, places to eat, packing suggestions, 
-                and a budget breakdown. You MUST suggest actual places to visit, actual hotels to stay, 
-                and actual restaurants to go to. This itinerary should cover all aspects of the trip, 
-                from arrival to departure, integrating the city guide information with practical travel logistics.
-
-            **Parameters**: 
-            - City: {city}
-            - Trip Date: {travel_dates}
-            - Traveler Interests: {interests}
-        """
+                f"""**Task**: Expand the city guide into a travel itinerary with detailed per-day plans, including weather forecasts, places to eat, packing suggestions, and a budget breakdown. You must suggest actual places to visit, actual hotels to stay, and actual restaurants to go to.
+                **Parameters**: 1. City: {city} 2. Trip Date: {travel_dates} 3. Traveler Interests: {interests}"""
             ),
             agent=agent,
-            expected_output=dedent("""Text"""),
+            expected_output=dedent("""Markdown"""),
             callback=lambda task_output: self.append_event_callback(
                 task_name, task_output
             ),
@@ -86,25 +33,11 @@ class TravelTasks:
         task_name = "identify_city"
         return Task(
             description=dedent(
-                f"""
-                    **Task**:  Identify the Best City for the Trip
-                    **Description**: Analyze and select the best city for the trip based on specific 
-                        criteria such as weather patterns, seasonal events, and travel costs. 
-                        This task involves comparing multiple cities, considering factors like current weather 
-                        conditions, upcoming cultural or seasonal events, and overall travel expenses. 
-                        Your final answer must be a detailed report on the chosen city, 
-                        including actual flight costs, weather forecast, and attractions.
-
-
-                    **Parameters**: 
-                    - Origin: {origin}
-                    - Cities: {cities}
-                    - Interests: {interests}
-                    - Travel Date: {travel_dates}
-                """
+                f"""**Task**:  Analyze and select the best city for the trip based on specific criteria such as weather patterns, seasonal events, and travel costs. This task involves comparing multiple cities, considering factors like current weather conditions, upcoming cultural or seasonal events, and overall travel expenses. Your final answer must be a detailed report on the chosen city, including actual flight costs, weather forecast, and attractions.
+                    **Parameters**: 1. Origin: {origin} 2. Cities: {cities} 3. Interests: {interests} 4. Travel Date: {travel_dates}"""
             ),
             agent=agent,
-            expected_output=dedent("""Text"""),
+            expected_output=dedent("""Markdown"""),
             callback=lambda task_output: self.append_event_callback(
                 task_name, task_output
             ),
@@ -114,21 +47,11 @@ class TravelTasks:
         task_name = "gather_city_info"
         return Task(
             description=dedent(
-                f"""
-                    **Task**:  Gather In-depth City Guide Information
-                    **Description**: Compile an in-depth guide for the selected city, gathering information about 
-                        key attractions, local customs, special events, and daily activity recommendations. 
-                        This guide should provide a thorough overview of what the city has to offer, including 
-                        hidden gems, cultural hotspots, must-visit landmarks, weather forecasts, and high-level costs.
-
-                    **Parameters**: 
-                    - Cities: {city}
-                    - Interests: {interests}
-                    - Travel Date: {travel_dates}
-                """
+                f"""**Task**: Compile an in-depth guide for the selected city, gathering information about key attractions, local customs, special events, and daily activity recommendations. This guide should provide a thorough overview of what the city has to offer, including hidden gems, cultural hotspots, must-visit landmarks, weather forecasts, and high-level costs.
+                    **Parameters**: 1. Cities: {city} 2. Interests: {interests} 3. Travel Date: {travel_dates}"""
             ),
             agent=agent,
-            expected_output=dedent("""Text"""),
+            expected_output=dedent("""Markdown"""),
             callback=lambda task_output: self.append_event_callback(
                 task_name, task_output
             ),
